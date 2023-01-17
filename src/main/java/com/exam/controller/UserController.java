f7ceeaee6f21dee89d0f7754fef1fd4e3fca7b77
Deepanshu Katiyar <deepanshukatiyar5656@gmail.com>
2023-01-17 22:30:25 +0530

    user service added

--- src/main/java/com/exam/controller/UserController.java
+++ src/main/java/com/exam/controller/UserController.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +42,16 @@ public class UserController {
         roles.add(userRole);
         
         return this.userService.createUser(user,roles);
+    }
+
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable("username") String username){
+        return this.userService.getUser(username);
+    }
+
+    @DeleteMapping("{userId}")
+    public void deletUser(@PathVariable("userId")Long userId){
+        this.userService.deleteUser(userId);
     }
 
 
